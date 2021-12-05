89370baff689e76154bc9de350f101b0839a71a3
iftakherone <54303402+iftakherone@users.noreply.github.com>
2021-12-05 22:21:34 +0600

    second commit

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ io.on('connect', (socket) => {
 
         socket.join(user.room);
 
+        io.to(user.room).emit('roomData', {room: user.room, users :getUsersInRoom(user.room)})
+
         callback();
     })
 
@@ -39,12 +41,18 @@ io.on('connect', (socket) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
 
         callback();
     })
 
     socket.on('disconnect', () => {
-        console.log('User left the chat!!!');
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left!`})
+        }
     })
 });
 
